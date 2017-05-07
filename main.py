d5961d53c837c463b0a801fb8d3c9884876f785e
ret702 <ret702@gmail.com>
2017-05-06 23:37:55 -0600

    pyscript
    python script to scape data from film website , working sample.

--- main.py
+++ main.py
@@ -10,18 +10,23 @@ def strip_non_ascii(string):
 
 page = requests.get("http://www.filmsite.org/filmterms1.html")
 html=BeautifulSoup(page.content,'html.parser')
-table_data=html.find_all("table")
+table=html.find_all("table")
 file=codecs.open(r'C:\Users\Owner\Videos\scraped_data.txt','wb', 'utf8')
 count=0
 td_count=0
 title_desc_map={}
-for t in table_data[2].find_all("tr"):
+for t in table[2].find_all("tr"):
     if(count>3):
-        for x in t.find_all("td"):
+        table_data=t.find_all("td")
+        if (len(table_data) == 3):
+            table_data[2].decompose()
+        for x in table_data:
             temp=x.text.replace("\n","").replace("\r","")
             newstring = re.sub('\s+', ' ', temp)
             title_desc_map[td_count]=strip_non_ascii(newstring)
+            file.write(title_desc_map[td_count] + "\n")
             td_count+=1
+
 
 
     count+=1
